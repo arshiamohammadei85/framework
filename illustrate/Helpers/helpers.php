9a304de28a1ccc103bd44a96254ad772236fcb4d
Arshiamohammadi <128997158+arshiamohammadei85@users.noreply.github.com>
2023-08-31 13:40:36 +0330

    update #composer

--- illustrate/Helpers/helpers.php
+++ illustrate/Helpers/helpers.php
@@ -7,7 +7,6 @@
  */
 
 use illustrate\Application;
-use illustrate\Blade;
 
 if (!function_exists('config')) {
     function config(string $name, string $file)
@@ -1844,5 +1843,299 @@ if (!function_exists('to_array')) {
     function to_array(string $json)
     {
         return (array)json_decode($json);
+    }
+}
+const JSON_TYPES = ['string', 'number', 'boolean', 'null', 'object', 'array'];
+const JSON_SUBTYPES = ['integer' => 'number'];
+const PHP_TYPE_MAP = [
+    'NULL' => 'null',
+    'integer' => 'integer',
+    'double' => 'number',
+    'boolean' => 'boolean',
+    'array' => 'array',
+    'object' => 'object',
+    'string' => 'string',
+];
+if (!function_exists('isValidJsonType')) {
+    function isValidJsonType(string $type): bool
+    {
+        if (isset(JSON_SUBTYPES[$type])) {
+            return true;
+        }
+
+        return in_array($type, JSON_TYPES, true);
+    }
+}
+if (!function_exists('getJsonSuperType')) {
+    function getJsonSuperType(string $type): ?string
+    {
+        return JSON_SUBTYPES[$type] ?? null;
+    }
+}
+if (!function_exists('getJsonType')) {
+    function getJsonType($value, bool $use_subtypes = true): ?string
+    {
+        $type = PHP_TYPE_MAP[gettype($value)] ?? null;
+        if ($type === null) {
+            return null;
+        } elseif ($type === 'array') {
+            return isIndexedArray($value) ? 'array' : null;
+        }
+
+        if ($use_subtypes) {
+            if ($type === 'number' && isMultipleOf($value, 1)) {
+                return 'integer';
+            }
+        } elseif ($type === 'integer') {
+            return 'number';
+        }
+
+        return $type;
+    }
+
+}
+if (!function_exists('jsonTypeMatches')) {
+    function jsonTypeMatches(string $type, $allowed): bool
+    {
+        if (!$allowed) {
+            return false;
+        }
+
+        if (is_string($allowed)) {
+            if ($type === $allowed) {
+                return true;
+            }
+
+            return $allowed === getJsonSuperType($type);
+        }
+
+        if (is_array($allowed)) {
+            if (in_array($type, $allowed, true)) {
+                return true;
+            }
+
+            if ($type = getJsonSuperType($type)) {
+                return in_array($type, $allowed, true);
+            }
+        }
+
+        return false;
+    }
+}
+if (!function_exists('valueIsOfJsonType')) {
+    function valueIsOfJsonType($value, $type): bool
+    {
+        $t = getJsonType($value);
+        if ($t === null) {
+            return false;
+        }
+
+        return jsonTypeMatches($t, $type);
+    }
+}
+if (!function_exists('isIndexedArray')) {
+    function isIndexedArray(array $array): bool
+    {
+        for ($i = 0, $max = count($array); $i < $max; $i++) {
+            if (!array_key_exists($i, $array)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
+if (!function_exists('convertAssocArrayToObject')) {
+    function convertAssocArrayToObject($schema)
+    {
+        if (is_null($schema) || is_scalar($schema)) {
+            return $schema;
+        }
+
+        $keepArray = is_array($schema) && self::isIndexedArray($schema);
+
+        $data = [];
+
+        foreach ($schema as $key => $value) {
+            $data[$key] = is_array($value) || is_object($value) ? self::convertAssocArrayToObject($value) : $value;
+        }
+
+        return $keepArray ? $data : (object)$data;
+    }
+}
+if (!function_exists('equals')) {
+    function equals($a, $b): bool
+    {
+        if ($a === $b) {
+            return true;
+        }
+
+        $type = getJsonType($a, false);
+        if ($type === null || $type !== getJsonType($b, false)) {
+            return false;
+        }
+
+        if ($type === 'number') {
+            return $a == $b;
+        }
+
+        if ($type === "array") {
+            $count = count($a);
+            if ($count !== count($b)) {
+                return false;
+            }
+
+            for ($i = 0; $i < $count; $i++) {
+                if (!array_key_exists($i, $a) || !array_key_exists($i, $b)) {
+                    return false;
+                }
+                if (!equals($a[$i], $b[$i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        if ($type === "object") {
+            $a = get_object_vars($a);
+            if ($a === null) {
+                return false;
+            }
+
+            $b = get_object_vars($b);
+            if ($b === null) {
+                return false;
+            }
+
+            if (count($a) !== count($b)) {
+                return false;
+            }
+
+            foreach ($a as $prop => $value) {
+                if (!array_key_exists($prop, $b)) {
+                    return false;
+                }
+                if (!equals($value, $b[$prop])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
+if (!function_exists('isMultipleOf')) {
+    function isMultipleOf($number, $divisor, int $scale = 14): bool
+    {
+        static $bcMath = null;
+        if ($bcMath === null) {
+            $bcMath = extension_loaded('bcmath');
+        }
+        if ($divisor == 0) {
+            return $number == 0;
+        }
+
+        if ($bcMath) {
+            $number = number_format($number, $scale, '.', '');
+            $divisor = number_format($divisor, $scale, '.', '');
+
+            /** @noinspection PhpComposerExtensionStubsInspection */
+            $x = bcdiv($number, $divisor, 0);
+            /** @noinspection PhpComposerExtensionStubsInspection */
+            $x = bcmul($divisor, $x, $scale);
+            /** @noinspection PhpComposerExtensionStubsInspection */
+            $x = bcsub($number, $x, $scale);
+
+            /** @noinspection PhpComposerExtensionStubsInspection */
+            return 0 === bccomp($x, 0, $scale);
+        }
+
+        $div = $number / $divisor;
+
+        return $div == (int)$div;
+    }
+
+}
+if (!function_exists('cloneValue')) {
+    function cloneValue($value)
+    {
+        if ($value === null || is_scalar($value)) {
+            return $value;
+        }
+
+        return null;
+    }
+}
+if (!function_exists('isValidPattern')) {
+    function isValidPattern(string $pattern): bool
+    {
+        if (strpos($pattern, '\Z') !== false) {
+            return false;
+        }
+
+        return @preg_match("\x07{$pattern}\x07u", '') !== false;
+    }
+
+}
+if (!function_exists('patternToRegex')) {
+    function patternToRegex(string $pattern): string
+    {
+        return "\x07{$pattern}\x07uD";
+    }
+}
+if (!function_exists('toJSON')) {
+    function toJSON($data)
+    {
+        if ($data === null || is_scalar($data)) {
+            return $data;
+        }
+
+        $map = [];
+
+        $isArray = true;
+        $index = 0;
+        foreach ($data as $key => $value) {
+            $map[$key] = toJSON($value);
+            if ($isArray) {
+                if ($index !== $key) {
+                    $isArray = false;
+                } else {
+                    $index++;
+                }
+            }
+        }
+
+        if ($isArray) {
+            if (!$map && is_object($data)) {
+                return (object)$map;
+            }
+            return $map;
+        }
+
+        return (object)$map;
+    }
+
+}
+if (!function_exists('app')) {
+    function app(string $object = null): Application
+    {
+        return Application::$app;
+    }
+}
+if (!function_exists('redirect')) {
+    function redirect($url): \illustrate\Response
+    {
+        $redirect = app()->make(\illustrate\Response::class);
+        return $redirect->redirect($url);
+    }
+}
+if (!function_exists('request')){
+    function request():\illustrate\Request
+    {
     }
 }
